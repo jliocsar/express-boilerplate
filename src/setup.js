a95aed814b7b16299f4dfafbf7c8a3c4b2376b85
jliocsar <juliocesarmfo@gmail.com>
2021-09-26 22:26:41 -0300

    chore: add npm usage error message

--- src/setup.js
+++ src/setup.js
@@ -1,5 +1,11 @@
 import { spawn } from 'child_process'
 
+import { isUsingYarn } from '~config/environment'
+
+if (!isUsingYarn) {
+  throw new Error('Please use Yarn to run this project')
+}
+
 export const runPrismaStudio = () => {
   spawn('yarn', ['studio'])
   console.info('Running Prisma Studio @ localhost:5555')
